3c9bc07f5819fc2d2a986a7ad57c074a1e7c0fe1
ShinDongJun <wq0212@naver.com>
2025-06-27 00:29:59 +0900

    feat: update vite.config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    outDir: "build", // 원하는 폴더명으로 변경 (예: 'build', 'public', 'release' 등)
+  },
+});
